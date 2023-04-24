167ed320da77de9f41ba3f39a94a5af696b1d687
Sah-Shamsuddin <soheluap@gmail.com>
2023-04-24 22:08:41 +0100

    update

--- assets/script.js
+++ assets/script.js
@@ -3,9 +3,16 @@ function updateClock() {
     const hours = now.getHours().toString().padStart(2, '0');
     const minutes = now.getMinutes().toString().padStart(2, '0');
     const seconds = now.getSeconds().toString().padStart(2, '0');
-    
+
     document.getElementById('hours').textContent = hours;
     document.getElementById('minutes').textContent = minutes;
     document.getElementById('seconds').textContent = seconds;
-  }
-  setInterval(updateClock, 1000);
+}
+setInterval(updateClock, 1000);
+
+date = new Date();
+year = date.getFullYear();
+month = date.getMonth() + 1;
+day = date.getDate();
+
+document.getElementById("current_date").innerHTML = day + "/" + month + "/" + year;
